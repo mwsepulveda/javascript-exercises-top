8389f8dabf315b2602a9b2aafa40608c3561d700
Michael <vergilxwolf@gmail.com>
2025-01-03 14:04:08 -0600

    Complete js exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    else if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+    else if (Math.floor(a) != a || Math.floor(b) != b) return "ERROR";
+
+    let start = a, end = b;
+    if (a > b) {
+        start = b;
+        end = a;
+    }
+    
+    let sum = 0;
+    while (start <= end) {
+        sum += start++;
+    }
+
+    return sum;
 
 };
 
