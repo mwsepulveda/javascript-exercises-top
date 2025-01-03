4aae62d006d21a396c73996473bf168e31372165
Michael <vergilxwolf@gmail.com>
2025-01-03 08:18:57 -0600

    Complete JS exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (arr[i] === arguments[j]) {
+                arr.splice(i--,1); // decrement i since there will be a new element in this index after deleting
+                break;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
