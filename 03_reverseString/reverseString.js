3810280d207ec587494daa5f6867ceaf026cda07
Michael <vergilxwolf@gmail.com>
2025-01-02 20:35:42 -0600

    Complete JS Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const chars = string.split("");
+    let left = 0, right = chars.length - 1;
+    while (left < right) {
+        let tmpLeft = chars[left];
+        chars[left] = chars[right];
+        chars[right] = tmpLeft;
+        left++;
+        right--;
+    }
 
+    return chars.join("");
 };
 
 // Do not edit below this line
